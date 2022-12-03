af980fe3721ee26a9169afdcc54f46c64b8b3f64
Duarte Laia <51859714+IDK04@users.noreply.github.com>
2022-12-03 12:16:52 +0000

    Delete 2021 directory

--- 2021/1/1.py
+++
@@ -1,9 +0,0 @@
-nums = []
-with open('input.txt', 'r') as file:
-    for line in file.readlines():
-        nums.append(int(line))
-
-increased = 0
-for i in range(1, len(nums)):
-    increased += 1 if nums[i] > nums[i-1] else 0
-print(increased)
